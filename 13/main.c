e9e8ded5b573601b4e5b58854f2f2eb56b72a5be
a-pinch <shmat.co@mail.ru>
2015-01-13 19:22:56 +0300

    1

---
+++ 13/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int gcd(int a, int b);
+int lcm(int a, int b);
+
+int main()
+{
+    int a,b;
+    printf("Input two numbers: ");
+    scanf("%d%d",&a,&b);
+    printf("gcd(%d,%d)=%d\n",a,b,gcd(a,b));
+    printf("lcm(%d,%d)=%d\n",a,b,lcm(a,b));
+    return 0;
+}
+
+
+int gcd(int a, int b){
+    if(b == 0) return a;
+    while(a%b != 0){
+        int c = a%b;
+        a = b; b = c;
+    }
+    return b;
+}
+
+int lcm(int a, int b){
+    return a * (b / gcd(a,b));
+}
