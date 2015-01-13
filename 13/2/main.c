49690fdacc4d62c5107459a74c7f17536c6c4d19
a-pinch <shmat.co@mail.ru>
2015-01-13 21:31:23 +0300

    2

---
+++ 13/2/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double binpow(int a, int p);
+
+int main()
+{
+    int a,p;
+    printf("Input number:");
+    scanf("%d",&a);
+    printf("Input power:");
+    scanf("%d",&p);
+    if(p<0){
+        printf("%d ^ %d = %.5f",a,p,binpow(a,p));
+    }else{
+        printf("%d ^ %d = %.0f",a,p,binpow(a,p));
+    }
+    return 0;
+}
+
+double binpow(int a, int p){
+    int res = 1, f=1;
+    if(p<0) {
+        f=0;
+        p = -p;
+    }
+    while(p){
+        if(p & 1){
+            res *= a;
+        }
+        a *= a;
+        p >>= 1;
+    }
+    return f ? res : (double)1/res;
+}
