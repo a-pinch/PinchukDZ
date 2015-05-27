fa1cc0e9400c65941d44843a7e66b07b2dcbfec4
a-pinch <shmat.co@mail.ru>
2015-05-27 10:38:41 +0300

    single list

--- C++/classwork/SingleList/SingleList/Source.cpp
+++ C++/classwork/SingleList/SingleList/Source.cpp
@@ -1,9 +1,16 @@
 #include "List.h";
+#include <iostream>
+
+using namespace std;
 
 int main(){
 	List l;
 	l.push_front(1).push_front(2).push_front(3);
 	l[1] = 7;
 
+	for (size_t i = 0; i < l.getSize(); i++){
+		cout << l[i] <<" ";
+	}
+	cin.get();
 	return 0;
 }
